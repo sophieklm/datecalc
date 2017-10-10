cad747b4601d2f719bb763a0f2148ad85225f55b
Sophie Mann <sklm@live.co.uk>
2017-10-10 13:17:20 +0100

    Add evaluator

--- datecalc.py
+++ datecalc.py
@@ -9,6 +9,9 @@ def parse(tokens):
     tok = tokens[0]
     return ("WordToken", tok[1])
 
+def evaluate(tree):
+    return ("DateValue", date.today())
+
 # -- tests --
 assert lex("today") == [("WordToken", "today")]
 
@@ -16,5 +19,16 @@ def p(x):
     return parse(lex(x))
 
 assert p("today") == ("WordToken", "today")
+
+def e(x):
+    return evaluate(parse(lex(x)))
+
+today = date.today()
+
+assert e("today") == ("DateValue", today)
+
+assert lex("tomorrow") == [("WordToken", "tomorrow")]
+
+assert p("tomorrow") == ("WordToken", "tomorrow")
 
 # -- main --
